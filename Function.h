39bcd663cbd967e3982c0d9e5a47fba424d8d071
David James <davidabraham@ucla.edu>
2019-04-29 20:34:06 -0700

    initial files added

---
+++ Function.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Function {
+ public:
+  Function(int power, double* coefficients);
+  double* getConstants();
+  int getPower();
+  double* getExtremum();
+  void findExtreme();
+  ~Function();
+ private:
+  double* bisectionMethod();
+  double* m_extremum;
+  double* m_constants;
+  double* m_x;
+  double* m_y;
+  int m_size;
+  int m_power;
+}
